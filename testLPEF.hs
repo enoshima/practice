a6c389d5412b4596bd5dab2a93f3769c4c92d8c7
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2014-09-22 23:50:25 +0900

    testing speed of whitening filter

--- testLPEF.hs
+++ testLPEF.hs
@@ -4,37 +4,41 @@
  - ghc--make testLPEF.hs
  -}
 
+import Data.List
+import Data.Time
+import qualified HasKAL.PlotUtils.HROOT.PlotGraph as HR
 import HasKAL.SignalProcessingUtils.ButterWorth
 --import HasKAL.SignalProcessingUtils.Chebyshev
 --import HasKAL.SignalProcessingUtils.Filter
-import HasKAL.SignalProcessingUtils.FilterType
+import qualified HasKAL.SignalProcessingUtils.FilterH as FH
+import HasKAL.SignalProcessingUtils.Filter
 import HasKAL.SignalProcessingUtils.LinearPrediction
+import HasKAL.SignalProcessingUtils.WindowFunction
 import HasKAL.SpectrumUtils.SpectrumUtils
 import HasKAL.SpectrumUtils.GwPsdMethod
-import HasKAL.SignalProcessingUtils.WindowFunction
-import HasKAL.SignalProcessingUtils.WindowFunction
 import HasKAL.FrameUtils.FrameUtils
-import qualified HasKAL.PlotUtils.HROOT.PlotGraph as HR
-import Data.List
-import Filter
-import Resampling
 import Numeric.LinearAlgebra
 import Numeric.GSL.Fourier
+import Resampling
+
 
 main = do
-
-
+  t1 <- getCurrentTime
   {- input data information -}
   let fname = "H-H2_RDS_C03_L2-877201786-128.gwf"
   ch <- getChannelList fname
   let [(chname,  _)]=ch
   fdata <- readFrame chname fname
   fs' <- getSamplingFrequency fname chname
+  t2 <- getCurrentTime
+  print $ diffUTCTime t2 t1
 
   {- downsampling -}
+  t3 <- getCurrentTime
   let fs = 4096 :: Double
       x = downsample fs' fs $ map realToFrac (eval fdata)
-
+  t4 <- getCurrentTime
+  print $ diffUTCTime t4 t3
   {- whitening filter properties -}
   let nfft = truncate fs
       nC =2048-1
@@ -51,8 +55,8 @@ main = do
       x_hpf = x
 
 
-
-  let anlylen = (truncate fs) * 5
+  t5 <- getCurrentTime
+  let anlylen = (truncate fs) * 20
       trlen = (truncate fs)
 
       {- set training data -}
@@ -62,22 +66,39 @@ main = do
 --      whnb = map (fs*) whnb'
       {- transfer function of whitening filter -}
       psd_whnb = gwpsd (whnb++(replicate (nfft-nC-1) 0)) nfft fs
-
+  t6 <- getCurrentTime
+  print $ diffUTCTime t6 t5
+  
+  t7 <- getCurrentTime
   let dat = take anlylen $ drop trlen $ x_hpf
       datlen = length dat
-      t = [y/fs |y<-[0.0..((fromIntegral datlen) -1.0)]] :: [Double]
+--      t = [y/fs |y<-[0.0..((fromIntegral datlen) -1.0)]] :: [Double]
       {- apply whitening filter -}
-      whitenedx = take anlylen $ drop nC $map (/sqrt rho) $ fir whnb x
+      whitenedx = take anlylen $ drop nC $map (/sqrt rho) $ FH.fir whnb x
+	  
 
-      newpsd = gwpsd whitenedx nfft fs
+--      newpsd = gwpsd whitenedx nfft fs
+  t8 <- getCurrentTime
+  print $ diffUTCTime t8 t7
 
-  print $ findIndices isNaN whitenedx
-  print $ findIndices isInfinite whitenedx
-  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "after hpf" "149022_afterhpf.png"  $ gwpsd trdat nfft fs
+  t9 <- getCurrentTime
+--  print $ findIndices isNaN whitenedx
+--  print $ findIndices isInfinite whitenedx
+--  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "after hpf" "149022_afterhpf.png"  $ gwpsd trdat nfft fs
 --  HR.plot HR.Linear HR.Line ("time", "amplitude") "after" "whiteningTest.png" $ zip t  whitenedx
 --  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "before" "whiteningTestbefore.png" $ psddat --gwpsd whitenedx 16384 16384
-  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "after" "149022_whiteningTestbafter.png" newpsd
-  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "transfer function" "149022_whiteningtransferFunction.png" psd_whnb
+--  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "after" "149022_whiteningTestbafter.png" newpsd
+--  HR.plot HR.LogXY HR.Line ("frequency", "Spectrum") "transfer function" "149022_whiteningtransferFunction.png" psd_whnb
+  print $ take 5 whitenedx
+  t10 <- getCurrentTime
+  print $ diffUTCTime t10 t9
 
-
+  t11 <- getCurrentTime
+  let whitenedx2 = take anlylen $ drop nC $ map (/sqrt rho) $ firFilter x whnb  
+  print $ take 5 whitenedx2
+  t12 <- getCurrentTime
+  print $ diffUTCTime t12 t11
+  
+  
+  
 
