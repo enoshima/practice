5ac905da9bc7a14f6e1c8311944efad4cd00f398
Kazuhiro Hayama <kazuhiro.hayama@gmail.com>
2015-09-23 00:16:46 +0900

    mplementing iir in parallel form added tf2cparallelForm for complex partial fraction decompposition, tf2rparallelForm for real partial fraction decomposition

--- parallelform/Function.hs
+++ parallelform/Function.hs
@@ -4,15 +4,14 @@
 module Function
 ( polyval
 , toeplitz
+, tf2cparallelForm
+, tf2rparallelForm
 ) where
 
 
 import Numeric.LinearAlgebra
 import Numeric.GSL.Polynomials(polySolve)
-import HasKAL.SignalProcessingUtils.ButterWorth
-import HasKAL.SignalProcessingUtils.Chebyshev
-import HasKAL.SignalProcessingUtils.FilterType
-import FilterKind
+import Data.Complex
 
 -- | calculating polynomials a0 + a1x^1+ .. + anx^n at x = x0
 -- | polyval [a0, a1, a2..an] x0
@@ -34,29 +33,55 @@ toeplitz c r = do
        | otherwise = (reverse . take i $ drop 1 y) ++ drop i c
 
 
-tf2parallelForm (num, denom) = do
+tf2rparallelForm :: ([Double], [Double]) -> ([Double], [([Double], [Double])])
+tf2rparallelForm (num, denom) = do
+  let (c, gain, alpha) = tf2cparallelForm (num, denom)
+   in (c, func gain alpha)
+
+func :: [Complex Double] -> [Complex Double] -> [([Double], [Double])]
+func _ [] = []
+func gain alpha = do
+  let hal = head alpha
+      hA  = head gain
+   in case (imagPart hal ==0) of
+        True -> ([2*realPart hA, -2*realPart (hA * conjugate hal)]
+          , [1, -2*realPart hal, (realPart (abs hal))**2]) : func (drop 2 gain) (drop 2 alpha)
+        False-> ([realPart (hA), 0], [1, -1*realPart hal, 0]) : func (tail gain) (tail alpha)
+
+
+
+tf2cparallelForm :: ([Double], [Double]) -> ([Double], [Complex Double], [Complex Double])
+tf2cparallelForm (num, denom) = do
   let p = length denom - 1
       q = length num - 1
-  (c, d) <- case (q >= p) of
-    True -> do
-      let temp = toeplitz (denom ++ replicate (q-p) 0::Double)
-            (head denom ++ repeat 0 (q-p))
-          numM = (q+1><1) num
-          denomM = (p+1><1) denom
-          zeros = replicate (q-p+1) (fromList $ replicate p 0::Double)
-          eye = ident p :: Matrix Double
-          temp' = fromBlocks [[denomM, fromRows (toRows eye ++ zeros)]]
-          temp''= temp' <\> numM
-          c = toList . head . toRows $ subMatrix (0, 0) (1, q-p+1) temp''
-          d = toList . head . toRows $ subMatrix (0, q-p+1) (1, p) temp''
-      return (c, d)
-    False -> do
-      let c = []
-          d = num ++ replicate (p-q+1) 0
-      return (c, d)
+      (c, d) = case (q >= p) of
+        True -> do
+          let temp = toeplitz (denom ++ replicate (q-p) (0::Double))
+                (head denom : replicate (q-p) (0::Double))
+              tempM = fromColumns $ map fromList temp
+              numM = ((q+1) >< 1) num
+              denomM = ((p+1) >< 1) denom
+              zeros = replicate (q-p+1) (fromList $ replicate p (0::Double))
+              eye = ident p :: Matrix Double
+              temp' = fromBlocks [[tempM, fromRows (toRows eye ++ zeros)]]
+              temp''= temp' <\> numM
+              c = toList . head . toRows $ subMatrix (0, 0) (1, q-p+1) temp''
+              d' = toList . head . toRows $ subMatrix (0, q-p+1) (1, p) temp''
+           in (c, d2clist d')
+        False -> do
+          let c = []
+              d' = num ++ replicate (p-q+1) (0::Double)
+           in (c, d2clist d')
 
   let alpha = polySolve $ reverse denom
-      gpf = map (\i -> product [alpha!!i - x | x <- alpha, x /= alpha!!i]) [0..length alpha -1]
+      gpf = map (\i->go i) [0..length alpha -1]
+        where
+          go i = do
+            let scale = product [alpha!!i - x | x <- alpha, x /= alpha!!i]
+             in (polyval d (alpha!!i)) / scale
+
+   in (c, gpf, alpha)
 
 
-
+d2clist :: [Double] -> [Complex Double]
+d2clist xs = map (\x->x:+0) xs
